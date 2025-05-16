4687e1c591f04efa733332aecb98a88f22440634
Runor Ewhro <rewhro@icloud.com>
2025-05-16 01:08:12 -0300

    cleared console logs

--- src/App.jsx
+++ src/App.jsx
@@ -417,9 +417,6 @@ export default function App() {
         combatState
     );
 
-    console.log("🟢 Merged Buffs:", mergedBuffs);
-    console.log("🟡 Final Stats:", finalStats);
-
     return (<>
             <SkillsModal skillsModalOpen={skillsModalOpen} setSkillsModalOpen={setSkillsModalOpen}
                          activeCharacter={activeCharacter} activeSkillTab={activeSkillTab}
